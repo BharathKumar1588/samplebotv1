b913bdec7cedcb259896d34041af6ed7ba545c66
bharath <bharathkumar1588@gmail.com>
2022-11-01 15:26:19 +0530

    updated bigquery code

--- webhook.py
+++ webhook.py
@@ -1,10 +1,14 @@
+from flask import Flask
+from flask import request
+from flask import make_response
+from google.cloud import bigquery
+
 import json
 import os
 import requests
 
-from flask import Flask
-from flask import request
-from flask import make_response
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]='mayabot_admin.json'
+
 
 # Flask app should start in global layout
 app = Flask(__name__)
@@ -13,37 +17,43 @@ app = Flask(__name__)
 def webhook():
 
     req = request.get_json(silent=True, force=True)
-    
+
     print(json.dumps(req, indent=4))
-    
+
     res = makeResponse(req)
-    
+
     res = json.dumps(res, indent=4)
     # print(res)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
-
 def makeResponse(req):
-    print("result is", req.get("result"))
-
-    print("type of request is",type(req))
 
     result = req.get("queryResult")
 
-    parameters = result.get("parameters")
-
-    city = parameters.get("geo-city")
-    date = parameters.get("date")
-    cars = parameters.get("cars")
-
-    print(f"city is {city} and date is {date} and cars type is {cars}")
+    speech = "something went wrong"
 
     action = result.get("action")
 
-    print(f"action is {action}")
+    if action = "bookcar":
+        parameters = result.get("parameters")
+
+        city = parameters.get("geo-city")
+        date = parameters.get("date")
+        cars = parameters.get("cars")
     
-    speech = f"city is {city} and date is {date} and cars type is {cars}"
+        print(f"city is {city} and date is {date} and cars type is {cars}")
+        print(f"action is {action}")
+        BigQuery_client = bigquery.Client()
+        query = "SELECT name[SAFE_OFFSET(0)] FROM `steel-signifier-364303.maya_data.Patient`"
+        query_job = BigQuery_client.query(query)
+
+        for row in query_job.result():
+            print(row)
+        
+        speech = f"city is {city} and date is {date} and cars type is {cars} and row is {row}"
+
+
 
     return {"fulfillmentMessages":[
         {
@@ -51,7 +61,7 @@ def makeResponse(req):
                 "text":[speech]
             }
         }]}
-    
+
     # return {
     # "speech": speech,
     # "displayText": speech,
@@ -62,20 +72,3 @@ if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
     print("Starting app on port %d" % port)
     app.run(debug=False, port=port, host='0.0.0.0')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
